91bea06524e65bf18b2ae13d36b10af11d6c1ce7
man999 <man9992012@ya.ru>
2026-04-13 20:16:29 +0300

    20260413T201600

---
+++ src/main/java/org/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package org.spring_data_rest.dao;
+
+import org.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
---
+++ src/main/java/org/spring_data_rest/entity/Employee.java
@@ -0,0 +1,36 @@
+package org.spring_data_rest.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+    @Column(name="department")
+    private String department;
+    @Column(name="salary")
+    private int salary;
+
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
